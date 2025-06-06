7d91fb3d52063245f6edde68a42e98bdef9cdf90
kaustav Mani Deka <chwapnamkashyap@gmail.com>
2025-06-06 23:44:40 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class", // Add this line to enable dark mode via class
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
